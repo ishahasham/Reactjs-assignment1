30b4bb49900dd9e246f9055bb56909c1e0591a55
Isha Hasham <144732430+ishahasham@users.noreply.github.com>
2024-06-28 20:17:57 +0500

    first reactjs assignment

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <div>
+    <h1>First React Assignment</h1>
+    <br />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, ipsam!</p>
+    <br />
+    <img src="/vite.svg" alt="" />
+    <form action="
+    ">
+      <input type="text" />
+      <button>Search</button>
+    </form>
+  </div>
+}
+
+export default App
